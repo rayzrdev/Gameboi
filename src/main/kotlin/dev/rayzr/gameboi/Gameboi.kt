fc390072ddb7dda6e2e084bcc660052074999396
Rayzr522 <rayzr522@gmail.com>
2019-06-24 18:49:28 -0400

    Added in config handler

--- src/main/kotlin/dev/rayzr/gameboi/Gameboi.kt
+++ src/main/kotlin/dev/rayzr/gameboi/Gameboi.kt
@@ -3,30 +3,51 @@ package dev.rayzr.gameboi
 import dev.rayzr.gameboi.command.Command
 import dev.rayzr.gameboi.command.HelpCommand
 import dev.rayzr.gameboi.command.PingCommand
+import dev.rayzr.gameboi.command.RenderTestCommand
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.events.GenericEvent
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.hooks.EventListener
+import org.omg.CORBA.Object
+import org.yaml.snakeyaml.Yaml
+import java.io.File
+import java.io.FileInputStream
+import java.nio.file.Files
+import kotlin.system.exitProcess
 
 fun main() {
     Gameboi.load()
 
-    val jda = JDABuilder("abc")
+    val jda = JDABuilder(Gameboi.token)
             .addEventListeners(Gameboi)
             .build()
 }
 
 object Gameboi : EventListener {
+    val yaml = Yaml()
     lateinit var prefix: String
+    lateinit var token: String
 
     fun load() {
-        // TODO: Load from config
-        prefix = "!"
+        val configFile = File("config.yml")
+        if (!configFile.exists()) {
+            Files.copy(javaClass.getResourceAsStream("config.yml"), configFile.toPath())
+
+            print("No config.yml found! The default one has been copied to the current directory. Please set it up before running Gameboi again.")
+            exitProcess(1)
+        }
+
+        val output = yaml.load(FileInputStream(configFile)) as Map<String, Object>
+        prefix = output["prefix"].toString()
+        token = output["token"].toString()
     }
 
     private val commands: List<Command> = listOf(
-            HelpCommand(),
-            PingCommand()
+            // Info
+            HelpCommand,
+            PingCommand,
+            // Test
+            RenderTestCommand
     )
 
     override fun onEvent(event: GenericEvent) {
@@ -39,7 +60,8 @@ object Gameboi : EventListener {
             val commandLabel = split[0]
             val args = split.slice(1..split.size)
 
-            commands.find { it.name == commandLabel }?.handle(event, args) ?: event.message.channel.sendMessage(":x: Invalid command `$commandLabel`!")
+            commands.find { it.name == commandLabel }?.handle(event, args)
+                    ?: event.message.channel.sendMessage(":x: Invalid command `$commandLabel`!")
         }
     }
 }
