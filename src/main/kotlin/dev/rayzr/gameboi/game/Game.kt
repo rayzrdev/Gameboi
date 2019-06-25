a75248cf0b4be3c21da085fec74c0aedc1a336be
Rayzr522 <rayzr522@gmail.com>
2019-06-24 21:43:25 -0400

    Updated game system

--- src/main/kotlin/dev/rayzr/gameboi/game/Game.kt
+++ src/main/kotlin/dev/rayzr/gameboi/game/Game.kt
@@ -7,6 +7,23 @@ import net.dv8tion.jda.api.entities.MessageReaction
 abstract class Game(private val width: Int, private val height: Int, val name: String, val maxPlayers: Int) {
     fun createRenderContext(match: Match): RenderContext = RenderContext(match.channel, width, height)
 
+    fun render(match: Match, reactions: List<String>, function: RenderContext.() -> Unit) {
+        match.renderContext.clear()
+        function.invoke(match.renderContext)
+        match.renderContext.draw {
+            addReactions(it, reactions)
+        }
+    }
+
+    fun addReactions(message: Message, reactions: List<String>) {
+//        if (reactions.isEmpty()) return
+//        message.addReaction(reactions[0]).queue {
+//            addReactions(message, reactions.slice(1 until reactions.size))
+//        }
+
+        reactions.forEach { message.addReaction(it).queue() }
+    }
+
     abstract fun begin(match: Match)
     abstract fun handleMessage(player: Player, match: Match, message: Message)
     abstract fun handleReaction(player: Player, match: Match, reaction: MessageReaction)
--- src/main/kotlin/dev/rayzr/gameboi/game/connect4/Connect4Game.kt
+++ src/main/kotlin/dev/rayzr/gameboi/game/connect4/Connect4Game.kt
@@ -5,16 +5,30 @@ import dev.rayzr.gameboi.game.Match
 import dev.rayzr.gameboi.game.Player
 import net.dv8tion.jda.api.entities.Message
 import net.dv8tion.jda.api.entities.MessageReaction
+import java.awt.Color
 
 object Connect4Game : Game(500, 400, "Connect 4", 2) {
-    val board: Array<Array<Slot>> = Array(6) {
-        Array(6) {
-            Connect4Game.Slot.EMPTY
-        }
-    }
+    val board: Array<Slot> = Array(36) { Connect4Game.Slot.EMPTY }
 
     override fun begin(match: Match) {
-        // TODO: Send initial frame
+        render(match, listOf("1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣")) {
+            clear()
+            graphics.run {
+                color = Color.BLUE
+                fillRect(50, 80, 400, 320)
+
+                val offsetX = 50
+                val offsetY = 80
+
+                board.forEachIndexed { index, slot ->
+                    val row = index % 6
+                    val col = index / 6
+
+                    color = slot.color
+                    fillOval(offsetX + 50 * col, offsetY + 50 * row, 50, 50)
+                }
+            }
+        }
     }
 
     override fun handleMessage(player: Player, match: Match, message: Message) {
@@ -25,9 +39,9 @@ object Connect4Game : Game(500, 400, "Connect 4", 2) {
         // TODO: Check which column, updated board, and re-send
     }
 
-    enum class Slot {
-        RED,
-        BLUE,
-        EMPTY
+    enum class Slot(val color: Color) {
+        RED(Color.RED),
+        BLUE(Color.BLUE),
+        EMPTY(Color.BLACK);
     }
 }
