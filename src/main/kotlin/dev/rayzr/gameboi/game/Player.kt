cbf9ab3f04f4ad34bc85b3dae81b722d2a68b89d
Rayzr522 <rayzr522@gmail.com>
2019-06-28 10:49:41 -0400

    Added timer to clear out old Player objects from players who aren't actively doing anything

--- src/main/kotlin/dev/rayzr/gameboi/game/Player.kt
+++ src/main/kotlin/dev/rayzr/gameboi/game/Player.kt
@@ -2,12 +2,27 @@ package dev.rayzr.gameboi.game
 
 import dev.rayzr.gameboi.data.DataManager
 import dev.rayzr.gameboi.data.PlayerData
+import dev.rayzr.gameboi.manager.InviteManager
 import net.dv8tion.jda.api.entities.User
+import java.util.*
+import java.util.concurrent.TimeUnit
+import kotlin.concurrent.scheduleAtFixedRate
+
+private val CACHE_CLEAR_DELAY = TimeUnit.MINUTES.toMillis(30)
 
 class Player private constructor(val user: User, var currentMatch: Match? = null) {
     companion object {
+        init {
+            Timer().scheduleAtFixedRate(CACHE_CLEAR_DELAY, CACHE_CLEAR_DELAY) {
+                players.filter {
+                    it.value.currentMatch == null && InviteManager.currentInvites.none { invite ->
+                        invite.to == it.value || invite.from == it.value
+                    }
+                }
+            }
+        }
+
         // Cache
-        // TODO: Clear cache at regular intervals for AFK players to avoid memory usage?
         private val players: MutableMap<Long, Player> = mutableMapOf()
 
         operator fun get(user: User) = players.computeIfAbsent(user.idLong) { Player(user) }
